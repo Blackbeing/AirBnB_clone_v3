7ba9c411df5647be5fe96491dcb29d1cfb5fa832
Blackbeing <johnjahi55@gmail.com>
2023-08-02 15:54:21 +0000

    fix  Minor corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
 This module defines a flask app and registers blueprint
-It serves at HBNB_API_HOST or 0.0.0.0, port HNBN_API_PORT or 5000
+It serves at HBNB_API_HOST or 0.0.0.0, port HBNB_API_PORT or 5000
 """
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,12 +14,11 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def purge_session(req):
+    """Remove/close a session at end of request"""
     storage.close()
 
 
 if __name__ == "__main__":
-    from os import getenv
-
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=port, threaded=True)
+    app.run(host, port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,16 +12,30 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from models import storage
+import socket
 
 
-@app_views.route("/status", methods=["GET"])
+def get_proc(port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(("0.0.0.0", port))
+            name = "socket.getservbyport({})".format(port)
+    except socket.timeout:
+        name = "No service running on {port}"
+    finally:
+        return name
+
+
+@app_views.route("/status", strict_slashes=False, methods=["GET"])
 def status():
     """Return a json object of dummy status code"""
-    return jsonify({"status": "OK"})
+    return jsonify({"port": get_proc(5050)})
+    # return jsonify({"status": "OK"}), 200
 
 
-@app_views.route("/stats", methods=["GET"])
+@app_views.route("/stats", strict_slashes=False, methods=["GET"])
 def stats():
+    """Return stats of each object as a json"""
     classes = {
         "Amenity": Amenity,
         "City": City,
@@ -30,4 +44,7 @@ def stats():
         "State": State,
         "User": User,
     }
-    return jsonify({k.lower(): storage.count(v) for k, v in classes.items()})
+    return (
+        jsonify({k.lower(): storage.count(v) for k, v in classes.items()}),
+        200,
+    )
