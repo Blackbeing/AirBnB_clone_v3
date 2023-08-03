7aa5221e4bf3fffc267238ebf7a36b378e7509ac
elcis2614 <elisereflorbusole@gmail.com>
2023-08-03 08:23:24 +0000

    get API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,5 +20,5 @@ def purge_session(req):
 if __name__ == "__main__":
     from os import getenv
     host = getenv("HBNB_API_HOST", default="0.0.0.0")
-    port = getenv("HBNB_API_PORT", default="5000")
+    port = getenv("HBNB_API_PORT", default="5050")
     app.run(host=host, port=port, threaded=True)
