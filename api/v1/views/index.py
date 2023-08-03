bcfd3dad6e075af40a48817d09c089033f10a977
elcis2614 <elisereflorbusole@gmail.com>
2023-08-03 10:05:29 +0000

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,11 +7,11 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status")
-@app_views.route("/status", strict_slashes=False, methods=["GET"])
+
+@app_views.route("/status", strict_slashes=False)
 def status():
     """Return a json object of dummy status code"""
-    return jsonify({"status": "OK"}), 200
+    return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats", strict_slashes=False)
