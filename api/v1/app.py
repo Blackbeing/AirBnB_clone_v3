46ab4c22dda8f1271796f0bb8ec4af005cc44cc7
elcis2614 <elisereflorbusole@gmail.com>
2023-08-03 08:59:28 +0000

    get .env

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,9 @@ def purge_session(req):
 
 if __name__ == "__main__":
     from os import getenv
+    from dotenv import load_dotenv
+    
+    load_dotenv()
     host = getenv("HBNB_API_HOST", default="0.0.0.0")
     port = getenv("HBNB_API_PORT", default="5000")
-    app.run(host=host, port=port, threaded=True, load_dotenv=True)
+    app.run(host=host, port=port, threaded=True)
