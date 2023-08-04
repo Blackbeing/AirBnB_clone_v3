06fda84c6a66929763f6c19615959824dcf71277
elcis2614 <elisereflorbusole@gmail.com>
2023-08-04 06:11:02 +0000

    including CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ It serves at HBNB_API_HOST or 0.0.0.0, port HBNB_API_PORT or 5000
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
