b44ba7a1b2fc7436687b9ed3db4099108294a2b5
elcis2614 <elisereflorbusole@gmail.com>
2023-08-07 12:06:29 +0000

    api status

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,14 @@
 This module defines a flask app and registers blueprint
 It serves at HBNB_API_HOST or 0.0.0.0, port HBNB_API_PORT or 5000
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import environ
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
@@ -17,6 +18,13 @@ app.register_blueprint(app_views)
 def purge_session(req):
     """Remove/close a session at end of request"""
     storage.close()
+
+
+@app.errorhandler(404)
+def notfound(err):
+    """handler for 404 errors
+        returns a JSON-formatted 404 status code response """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
